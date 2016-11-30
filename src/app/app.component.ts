686e6cce9911788374ab97b3848e2696c7a37d9b
Daniel Becerra <danieljosebecerra@gmail.com>
2016-11-30 01:46:49 -0430

    property binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: 'app-root',  
+  template: `
+  <h1>Este es un inline</h1>
+  <app-databinding></app-databinding>    
+  `,
+  styles: [`
+  h1 {
+    color: red;
+  }
+  `],
+    
 })
 export class AppComponent {
-  title = 'app works!';
+  
 }
