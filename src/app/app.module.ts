cb25ebecdaf70baad34895f26ea4aac82da8bb93
Daniel Becerra <danieljosebecerra@gmail.com>
2016-12-13 12:26:20 -0430

    event binding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,7 @@ import { AnotherComponent } from './another/another.component';
 import { OtrosComponent } from './other/otros.component';
 import { DatabindingComponent } from './databinding/databinding.component';
 import { PropertyBindingComponent } from './databinding/property-binding.component';
+import { EventBindingComponent } from './databinding/event-binding.component';
 
 @NgModule({
   declarations: [
@@ -17,7 +18,8 @@ import { PropertyBindingComponent } from './databinding/property-binding.compone
     AnotherComponent,
     OtrosComponent,
     DatabindingComponent,
-    PropertyBindingComponent
+    PropertyBindingComponent,
+    EventBindingComponent
   ],
   imports: [
     BrowserModule,
